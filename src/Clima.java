12cf50eb178de43b96604d122b5e6fe36a957997
serraguti <pacoserranox@gmail.com>
2025-06-18 12:26:03 +0200

    version 1

---
+++ src/Clima.java
@@ -0,0 +1,11 @@
+import com.objetos.Mes;
+
+public class Clima {
+    public static void main(String[] args) {
+        Mes mes = new Mes();
+        mes.setNombre("JUNIO");
+        mes.setTemperaturaMaxima(38);
+        mes.setTemperaturaMinima(20);
+        System.out.println(mes);
+    }
+}
