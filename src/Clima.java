94abe7deec5890a4fa307daeb6cc2b84404e9a3c
serraguti <pacoserranox@gmail.com>
2025-06-18 13:08:50 +0200

    codigo base

--- src/Clima.java
+++ src/Clima.java
@@ -1,11 +1,23 @@
 import com.objetos.Mes;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Clima {
     public static void main(String[] args) {
-        Mes mes = new Mes();
-        mes.setNombre("JUNIO");
-        mes.setTemperaturaMaxima(38);
-        mes.setTemperaturaMinima(20);
-        System.out.println(mes);
+        Scanner teclado = new Scanner(System.in);
+        ArrayList<Mes> meses = new ArrayList<Mes>();
+        //BUCLE PARA ALMACENAR MESES
+        for (int i = 1; i <= 12; i++){
+            Mes mes = new Mes();
+            mes.setNombre("JUNIO " + i);
+            mes.setTemperaturaMaxima(38);
+            mes.setTemperaturaMinima(20);    
+            meses.add(mes);
+        }
+        //BUCLE PARA DIBUJAR LOS MESES
+        for (Mes m : meses){
+            System.out.println(m);
+        }
+        System.out.println("Fin de programa");
     }
 }
