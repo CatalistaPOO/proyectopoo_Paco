eb79e12c24f200e0a73e8df87d8c37026e0b7155
serraguti <pacoserranox@gmail.com>
2025-06-18 10:24:09 +0200

    acelerar error

--- src/Conductor.java
+++ src/Conductor.java
@@ -1,19 +1,33 @@
-import com.objetos.Coche;
+import com.objetos.Deportivo;
 
 public class Conductor {
     public static void main(String[] args) {
-        Coche car = new Coche();
-        car.setMarca("Seat");
-        car.setModelo("Panda");
+        Deportivo car = new Deportivo();
+        car.setMarca("Rayo");
+        car.setModelo("McQueen");
         car.arrancar(); //arrancamos
-        car.acelerar(); //20
-        car.acelerar(); //40
-        car.acelerar(); //60
-        car.frenar(); // 50
-        car.frenar(); //40
+        car.acelerar(); //50
+        car.acelerar(); //100
+        car.acelerar(); //150
+        car.turbo(); //190
+        car.frenar(); // 180
+        car.frenar(); // 170
         car.frenar(true); //0 y apagado
-        //Si intentamos acelerar después...
-        car.acelerar(); //no podemos acelerar
+        // //Si intentamos acelerar después...
+        car.acelerar(); //El deportivo acelera aunque esté apagado...
         System.out.println(car);
+        // Coche car = new Coche();
+        // car.setMarca("Seat");
+        // car.setModelo("Panda");
+        // car.arrancar(); //arrancamos
+        // car.acelerar(); //20
+        // car.acelerar(); //40
+        // car.acelerar(); //60
+        // car.frenar(); // 50
+        // car.frenar(); //40
+        // car.frenar(true); //0 y apagado
+        // //Si intentamos acelerar después...
+        // car.acelerar(); //no podemos acelerar
+        // System.out.println(car);
     }
 }
